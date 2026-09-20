cd573eff9e600a1dee40ef08e5f70564f51f9b5c
Camilo Ruiz Alquichire <camiloruizroa06@gmail.com>
2026-09-20 13:07:53 -0500

    Cambios iniciales

---
+++ sw.js
@@ -0,0 +1,28 @@
+// Sube el número de versión cada vez que cambies index.html
+var CACHE='mi-reventa-v2';
+var SHELL=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png','apple-touch-icon.png'];
+
+self.addEventListener('install',function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(SHELL);}).then(function(){return self.skipWaiting();}));
+});
+self.addEventListener('activate',function(e){
+  e.waitUntil(caches.keys().then(function(ks){
+    return Promise.all(ks.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
+  }).then(function(){return self.clients.claim();}));
+});
+self.addEventListener('fetch',function(e){
+  var r=e.request;
+  if(r.method!=='GET')return;
+  var u=new URL(r.url);
+  var fonts=u.hostname==='fonts.googleapis.com'||u.hostname==='fonts.gstatic.com';
+  if(u.origin!==location.origin&&!fonts)return;
+  e.respondWith(caches.open(CACHE).then(function(c){
+    return c.match(r,{ignoreSearch:true}).then(function(hit){
+      var net=fetch(r).then(function(res){
+        if(res&&(res.ok||res.type==='opaque'))c.put(r,res.clone());
+        return res;
+      }).catch(function(){return hit;});
+      return hit||net;
+    });
+  }));
+});
